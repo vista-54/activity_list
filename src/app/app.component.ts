d5bf883305b59e05a33e597d92aaea9947f8ae06
Vitalii Stalynskyi <vitaliistalynskyi@MacBook-Pro-Vitalii.local>
2020-06-20 16:07:42 +0300

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'activity-app';
+  data: any;
+  templates: any;
+
+  constructor() {
+    this.data = [
+      {
+        'category': 'DEAL_ROOM',
+        'event': 'ADDED',
+        'username': 'vista',
+        'room_title': 'my Room',
+        'date': '01-02-2020'
+      },
+      {
+        'category': 'DEAL_ROOM',
+        'event': 'RENAMED',
+        'username': 'vista',
+        'room_title': 'my Room new',
+        'date': '01-02-2020'
+      },
+      {
+        'category': 'DEAL_ROOM',
+        'event': 'ARCHIVED',
+        'username': 'vista',
+        'room_title': 'my Room new',
+        'date': '01-02-2020'
+      },
+      {
+        'category': 'DEAL_ROOM_MEMBERS',
+        'event': 'ADDED',
+        'username': 'vista',
+        'username2': 'Valik',
+        'room_title': 'my Room new',
+        'date': '01-02-2020'
+      }
+
+    ];
+    this.templates = {
+      DEAL_ROOM: {
+        ADDED: (data) => {
+          return `${data.username} added ${data.room_title} deal room on ${data.date}`;
+        },
+        RENAMED: (data) => {
+          return `${data.username} renamed this deal room to ${data.room_title}  on ${data.date}`;
+        },
+        ARCHIVED: (data) => {
+          return `${data.username} archived ${data.room_title} deal room on ${data.date}`;
+        }
+      },
+      DEAL_ROOM_MEMBERS: {
+        ADDED: (data) => {
+          return `${data.username} added ${data.username2} to this deal room on ${data.date}`;
+        }
+      }
+    }
+    ;
+  }
 }
